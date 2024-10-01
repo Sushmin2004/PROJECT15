01e2b6a47e06d5fb61d48fcf7f2c2457f7807d49
Sushmin <sushmin2004@gmail.com>
2024-10-01 16:47:08 +0530

    website created

---
+++ main.js
@@ -0,0 +1,5 @@
+function combine() {
+    var a = document.getElementById("first").value
+    var b = document.getElementById("last").value
+    document.getElementById("full").innerHTML = a + " " + b
+}
